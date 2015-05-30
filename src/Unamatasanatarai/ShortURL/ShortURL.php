7dc0582d131815df7493f533d8e93fba1f9ad8e4
unamatasanatarai <unamatasanatarai@gmail.com>
2015-05-30 07:25:01 +0200

    automagic

---
+++ src/Unamatasanatarai/ShortURL/ShortURL.php
@@ -0,0 +1,22 @@
+<?php namespace Unamatasanatarai\ShortURL;
+
+/**
+ * A PHP port of URLify.js from the Django project
+ * (https://github.com/django/django/blob/master/django/contrib/admin/static/admin/js/urlify.js).
+ * Handles symbols from Latin languages, Greek, Turkish, Russian, Ukrainian,
+ * Czech, Polish, and Latvian. Symbols it cannot transliterate
+ * it will simply omit.
+ *
+ * Usage:
+ *
+ *     echo URLify::filter (' J\'étudie le français ');
+ *     // "jetudie-le-francais"
+ *
+ *     echo URLify::filter ('Lo siento, no hablo español.');
+ *     // "lo-siento-no-hablo-espanol"
+ */
+class ShortURL {
+
+}
+
+?>
---
+++ src/Unamatasanatarai/ShortURL/ShortURLServiceProvider.php
@@ -0,0 +1,26 @@
+<?php namespace Unamatasanatarai\ShortURL;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShortURLServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
